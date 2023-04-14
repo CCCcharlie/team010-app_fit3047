fd4942726c98bab8ae312ef520d0d33d56ff3ffa
adun0008 <adun0008@student.monash.edu>
2023-04-14 22:24:35 +1000

    Starting the changes to home.php
    Moving bootstrap elements and css changes over as well.

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -45,5 +45,24 @@ $this->disableAutoLayout();
     <?= $this->fetch('script') ?>
 </head>
 <body>
-
+<body id="page-top">
+<!-- Navigation-->
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+    <div class="container">
+        <a class="navbar-brand" href="#page-top"><img src="img\holistichealinglogofull.png" alt="Holistic Healing" /></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            Menu
+            <i class="fas fa-bars ms-1"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
+                <li class="nav-item"><a class="nav-link" href="#services">About</a></li>
+                <li class="nav-item"><a class="nav-link" href="#portfolio">Gallery</a></li>
+                <li class="nav-item"><a class="nav-link" href="#about">Services</a></li>
+                <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+</body>
 </html>
