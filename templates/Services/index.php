0547f9f7f45e130a234d9d46c751daf2e2ef72d2
bbra0010 <bbra0010@student.monash.edu>
2023-04-14 23:49:48 +1000

    services is UNFINISHED, I set up as much as I possibly could for the skeleton of the card, changes are most certainly welcomed

--- templates/Services/index.php
+++ templates/Services/index.php
@@ -4,6 +4,8 @@
  * @var iterable<\App\Model\Entity\Service> $services
  */
 ?>
+
+
 <div class="services index content">
     <?= $this->Html->link(__('New Service'), ['action' => 'add'], ['class' => 'button float-right']) ?>
     <h3><?= __('Services') ?></h3>
@@ -50,5 +52,45 @@
             <?= $this->Paginator->last(__('last') . ' >>') ?>
         </ul>
         <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
+    </div>
+</div>
+
+
+<!-- Essentially tells index.php to use bootstrap.css -->
+<?= $this->Html->css('bootstrap.css')?>
+
+
+<div class="content">
+    <div class="container">
+        <div class="row">
+            <!-- align items stretch aligns the item to "--bs-card-height: 350px;"-->
+            <!-- LOOP HERE -->
+            <?php foreach ($services as $service): ?>
+            <div class="col-xs-12 col-sm-4 d-flex align-items-stretch">
+                <div class="card">
+                    <!-- $viewURL acts as a temporary variable to store the path of each created card so it can redirect
+                    when clicked-->
+                    <?php $viewURL = "/services/view/".$service->service_id?>
+
+                    <!-- Image section -->
+                    <a class="card-img" href="<?= $this->Url->build($viewURL) ?>" style="object-fit: fill">
+                          <?= @$this->Html->image($service->image_name) ?>
+                    </a>
+                    <div class="card-content">
+                        <!-- Title section -->
+                        <h4 class="card-title">
+                            <a href="<?= $this->Url->build($viewURL) ?>">
+                                <?= h($service->service_name) ?>
+                            </a>
+                        </h4>
+                        <!-- Description section -->
+                        <p>
+                            <?= h($service->service_desc) ?>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <?php endforeach; ?>
+        </div>
     </div>
 </div>
