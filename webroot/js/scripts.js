8959249805d0b0ada1ad03330cab1531f0da6f10
adun0008 <adun0008@student.monash.edu>
2023-04-15 00:01:37 +1000

    Added a lightbox-based gallery I found online (attributed etc. Should do a full verification license at the end)
    Modified it to stop it's rotation, removed some unecessary features. Will update the images and iconography to be more inline with the websites theme tommorrow.
    
    Addition: Added background colour for the navigation bar. Added logo to navigation bar, added icon. Logo made in Paint3D and Canva.

--- webroot/js/scripts.js
+++ webroot/js/scripts.js
@@ -28,6 +28,50 @@ window.addEventListener('DOMContentLoaded', event => {
 
     // Shrink the navbar when page is scrolled
     document.addEventListener('scroll', navbarShrink);
-    }
+})
     //  Activate Bootstrap scrollspy on the main nav element
 
+    // Gallery JS -  Copyright (c) 2023 Jaden C Bathon (https://codepen.io/InfernoHoundDEV/pen/KKaeQpX). Free for commercial use under the MIT License.
+$("[id^=carousel-thumbs]").carousel({
+    interval: false
+});
+
+// Added to stop the top carousel form playing.
+$("[id^=carousel]").carousel({
+    interval: false
+});
+// No longer needed.
+// $(".carousel-pause").click(function () {
+//     var id = $(this).attr("href");
+//     if ($(this).hasClass("pause")) {
+//         $(this).removeClass("pause").toggleClass("play");
+//         $(this).children(".sr-only").text("Play");
+//         $(id).carousel("pause");
+//     } else {
+//         $(this).removeClass("play").toggleClass("pause");
+//         $(this).children(".sr-only").text("Pause");
+//         $(id).carousel("pause");
+//     }
+//     $(id).carousel;
+// });
+
+// Commented out, prefer to have the icons there constantly.
+// if ($("[id^=carousel-thumbs] .carousel-item").length < 50) {
+//     $("#carousel-thumbs [class^=carousel-control-]").remove();
+//     $("#carousel-thumbs").css("padding", "0 5px");
+// }
+
+
+// Moves the bottom carosouel alongside the top images.)
+$("#carousel").on("slide.bs.carousel", function (e) {
+    var id = parseInt($(e.relatedTarget).attr("data-slide-number"));
+    var thumbNum = parseInt(
+        $("[id=carousel-selector-" + id + "]")
+            .parent()
+            .parent()
+            .attr("data-slide-number")
+    );
+    $("[id^=carousel-selector-]").removeClass("selected");
+    $("[id=carousel-selector-" + id + "]").addClass("selected");
+    $("#carousel-thumbs").carousel(thumbNum);
+});
