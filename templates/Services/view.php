cd33619b10c0c30597238da07c758054dc6aaae1
adun0008 <adun0008@student.monash.edu>
2023-04-17 01:09:22 +1000

    User-proofed the Services and View templates. Will look towards making whole new pages for iteration 2.

--- templates/Services/view.php
+++ templates/Services/view.php
@@ -3,15 +3,56 @@
  * @var \App\View\AppView $this
  * @var \App\Model\Entity\Service $service
  */
+$this->disableAutoLayout();
+$cakeDescription = 'Holistic Healing - Service Showcase View';
 ?>
+
+<head>
+    <?= $this->Html->charset() ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>
+        <?= $cakeDescription ?>:
+        <?= $this->fetch('title') ?>
+    </title>
+    <?= $this->Html->meta('icon') ?>
+
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oxygen&display=swap" rel="stylesheet">
+
+    <?= $this->Html->css(['normalize.min', 'milligram.min', 'cake']) ?>
+    <?= $this->fetch('meta') ?>
+    <?= $this->fetch('css') ?>
+    <?= $this->fetch('script') ?>
+</head>
+
+<body>
+<nav class="top-nav">
+    <div class="top-nav-title">
+        <!--  In order to show the image from webroot/img/cake.icon.png,
+        we must use $this->html->image instead of <img src=""> in this case for it to work -->
+
+        <!-- <?php /*= "$this->Url->build('/')" */?> <- this line of code is for redirection, "/" is the root path-->
+        <a href="<?= $this->Url->build('/') ?>"> <?= $this->Html->image('holistichealinglogo.png', ['alt' => 'Holistic healing logo']); ?>
+            <a href="<?= $this->Url->build('/') ?>">Holistic<span> Healings</a>
+    </div>
+    </div>
+</nav>
+<main class="main">
+    <div class="container">
+        <?= $this->Flash->render() ?>
+        <?= $this->fetch('content') ?>
+    </div>
+
 <div class="row">
     <aside class="column">
         <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Html->link(__('Edit Service'), ['action' => 'edit', $service->service_id], ['class' => 'side-nav-item']) ?>
-            <?= $this->Form->postLink(__('Delete Service'), ['action' => 'delete', $service->service_id], ['confirm' => __('Are you sure you want to delete # {0}?', $service->service_id), 'class' => 'side-nav-item']) ?>
-            <?= $this->Html->link(__('List Services'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-            <?= $this->Html->link(__('New Service'), ['action' => 'add'], ['class' => 'side-nav-item']) ?>
+<!--            <h4 class="heading">--><?php //= __('Actions') ?><!--</h4>-->
+<!--            --><?php //= $this->Html->link(__('Edit Service'), ['action' => 'edit', $service->service_id], ['class' => 'side-nav-item']) ?>
+<!--            --><?php //= $this->Form->postLink(__('Delete Service'), ['action' => 'delete', $service->service_id], ['confirm' => __('Are you sure you want to delete # {0}?', $service->service_id), 'class' => 'side-nav-item']) ?>
+<!--            --><?php //= $this->Html->link(__('List Services'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+<!--            --><?php //= $this->Html->link(__('New Service'), ['action' => 'add'], ['class' => 'side-nav-item']) ?>
         </div>
     </aside>
     <div class="column-responsive column-80">
@@ -42,3 +83,8 @@
         </div>
     </div>
 </div>
+</main>
+<footer>
+</footer>
+</body>
+</html>
