2a250fdb7a1761a837bf49cc53db109f18a2e230
adun0008 <adun0008@student.monash.edu>
2023-04-22 23:16:40 +1000

    Quick mistake fix on Enquiry Table. Changed enquiry text colour to white.

--- src/Model/Table/EnquiryTable.php
+++ src/Model/Table/EnquiryTable.php
@@ -91,7 +91,7 @@ class EnquiryTable extends Table
             ]);
         $validator
             ->scalar('Message')
-            ->maxLength('Message', 50)
+            ->maxLength('Message', 500)
             ->requirePresence('Message', 'create')
             ->notEmptyString('Message');
 
