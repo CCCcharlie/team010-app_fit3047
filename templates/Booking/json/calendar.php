bf57e5300bab16fa8935a4080edd828f3ef2123a
adun0008 <adun0008@student.monash.edu>
2023-05-08 22:11:27 +1000

    Added a way to concatinate titles based on the elements of the booking.

--- templates/Booking/json/calendar.php
+++ templates/Booking/json/calendar.php
@@ -1,15 +1,21 @@
 <?php
 $data = [];
 foreach ($booking as $booking) {
+    $service_name = $booking->service->service_name;
+    $customer_name = substr($booking->customer->cust_fname, 0, 1) . '. ' . $booking->customer->cust_lname;
+    $staff_name = substr($booking->staff->staff_fname, 0,1) . '. ' . $booking->staff->staff_lname;
+    $title = $service_name . ' - ' . $customer_name . ' with ' . $staff_name;
+
+
     $eventStart = new DateTime($booking->eventstart, new DateTimeZone('UTC'));
     $eventEnd = new DateTime($booking->eventend, new DateTimeZone('UTC'));
+
     $item = [
-        'title' => $booking->title,
+        'title' => $title,
         'start' => $eventStart->format('Y-m-d\TH:i:s'),
         'end' => $eventEnd->format('Y-m-d\TH:i:s'),
-        'timeZone' => 'UTC', // Specify the time zone explicitly. Stops it from converting over.
+        'timeZone' => 'UTC', // Specify the time zone explicitly
     ];
     $data[] = $item;
 }
 echo json_encode($data);
-
