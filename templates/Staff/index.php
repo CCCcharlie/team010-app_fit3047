0508ef7b999db5693865277018956c7b18976ec1
adun0008 <adun0008@student.monash.edu>
2023-05-18 09:25:46 +1000

    Removed needless lines.

--- templates/Staff/index.php
+++ templates/Staff/index.php
@@ -32,10 +32,10 @@
             </button>
             <div class="dropdown-content">
 
-                <a target="_self"  href="<?= $this->Url->build('/enquiry') ?>">Customer Enquiries</a> |
-                <a target="_self"  href="<?= $this->Url->build('/services/admindex') ?>">Service List</a> |
-                <a target="_self"  href="<?= $this->Url->build('/booking') ?>">Bookings</a> |
-                <a target="_self"  href="<?= $this->Url->build('/staff') ?>">Staff Overview</a> |
+                <a target="_self"  href="<?= $this->Url->build('/enquiry') ?>">Customer Enquiries</a>
+                <a target="_self"  href="<?= $this->Url->build('/services/admindex') ?>">Service List</a>
+                <a target="_self"  href="<?= $this->Url->build('/booking') ?>">Bookings</a>
+                <a target="_self"  href="<?= $this->Url->build('/staff') ?>">Staff Overview</a>
             </div>
         </div>
 
